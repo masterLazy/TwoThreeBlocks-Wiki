b9fb83a9eced6a5a761fd768f64d69d4782ffd4b
mLazy <msterlazy@qq.com>
2025-04-26 15:21:07 +0800

    Initial commit

---
+++ assets/js/5e95c892.1386ddb1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkttb_wiki=self.webpackChunkttb_wiki||[]).push([[647],{8122:(e,s,r)=>{r.r(s),r.d(s,{default:()=>k});r(6540);var t=r(4164),u=r(1769),a=r(204),c=r(2831),i=r(1410),n=r(4848);function k(e){return(0,n.jsx)(u.e3,{className:(0,t.A)(a.G.wrapper.docsPages),children:(0,n.jsx)(i.A,{children:(0,c.v)(e.route.routes)})})}}}]);
